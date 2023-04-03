25e07f6ed697076dc02027ba5f5bc6e27ec88e34
Pranil-Meherkar <pranil@nimapinfotech.com>
2023-04-03 17:11:50 +0530

    added a layout for admin panel

---
+++ src/components/layout/HomeLayout.js
@@ -0,0 +1,34 @@
+import React, {useState, lazy} from 'react';
+import { Outlet } from 'react-router-dom';
+// import Sidebar from './../components/dashboard/Sidebar';
+// import Navbar from '../components/dashboard/Navbar';
+// import ProfileModal from '../components/dashboard/modal/ProfileModal';
+
+const Sidebar = lazy(() => import("./../components/dashboard/Sidebar"))
+const Navbar = lazy(() => import("./../components/dashboard/Navbar"))
+const ProfileModal = lazy(() => import("../components/dashboard/modal/ProfileModal"))
+
+
+const HomeLayout = ({setToken}) => {
+    const [hideSidebar, sethideSidebar] = useState(false);
+    const handleHideSidebar = () => {
+        sethideSidebar(!hideSidebar)
+    }
+    const [openModal, setOpenModal] = useState(false);
+    
+    return ( 
+        <div className='home-layout'>
+            {openModal && <ProfileModal setToken={setToken} username={JSON.parse(localStorage.getItem("user"))?.email}/>}
+            
+            <Sidebar hideSidebar={hideSidebar} handleHideSidebar={handleHideSidebar}/>
+            <div className='homepage-container'>
+                <Navbar openModal={openModal} setOpenModal={setOpenModal} setToken={setToken} username={JSON.parse(localStorage.getItem("user"))?.email} />
+                <div>
+                    <Outlet/>
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default HomeLayout;
