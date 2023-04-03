17b05b2a9e2ce4ae72fbdbc039dad1aeb8138431
Pranil-Meherkar <pranil@nimapinfotech.com>
2023-04-03 15:59:05 +0530

    updated the folder structure

---
+++ src/components/formikcontrols/Password.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Field, ErrorMessage } from 'formik'
+import TextError from '../shared/TextError'
+import { TextField } from '@mui/material'
+
+const Password = (props) => {
+    const { label, name, icon, ...rest } = props
+    return (
+        <div className='form-control'>
+            <label htmlFor={name}>{icon}</label>
+            <Field name={name} {...rest}>
+                {
+                    ({field})=>{
+                        return(
+                            <TextField 
+                            {...field}
+                            type='password'
+                            id={name}
+                            label={label}
+                            variant="standard"
+                            className='input-field' 
+                            required
+                            />
+                        )
+                    }
+                }
+            </Field>
+            <ErrorMessage name={name} component={TextError}/>
+        </div>
+    )
+}
+
+export default Password
