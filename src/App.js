e8f04d0f3fdeac7752deea4deb00118cbda6ab3b
Pranil-Meherkar <pranil@nimapinfotech.com>
2023-04-05 10:39:43 +0530

    implemented lazy loading

--- src/App.js
+++ src/App.js
@@ -1,36 +1,24 @@
 import React, { useState, useEffect, lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router'
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css'
 import { ThemeProvider, createTheme } from '@mui/material'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import MainHeader from './components/layout/MainHeader'
-import Dashboard from './components/pages/dashboard/Dashboard'
 import Error from './components/pages/ErrorPage/Error'
-import ProtectedRoutes from './routes/ProtectedRoutes'
 import TestPage from './components/pages/TestPage'
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import Loader from './components/shared/Loader';
 import PrivateRoute from './routes/PrivateRoute';
 import PublicRoute from './routes/PublicRoute';
 
-
-
-// const Login = lazy(() => import("./components/login/Login"))
-// // const Dashboard = lazy(() => import("./pages/Dashboard"))
-// const Roles = lazy(() => import("./components/pages/roles/Roles"))
-// const HomeLayout = lazy(() => import("./layouts/HomeLayout"))
-// const Permissions = lazy(() => import("./components/pages/permissions/Permissions"))
-// const Profiles = lazy(() => import("./components/pages/profiles/Profiles"))
-// const Settings = lazy(() => import("./components/pages/settings/Settings"))
-// const BulkUpload = lazy(() => import("./components/pages/bulkupload/BulkUpload"))
-import HomeLayout from './components/layout/HomeLayout';
-import Permissions from './components/pages/permissions/Permissions';
-import Roles from './components/pages/roles/Roles';
-import Profiles from './components/pages/profiles/Profiles';
-import Settings from './components/pages/settings/Settings';
-import BulkUpload from './components/pages/bulkupload/BulkUpload';
-import Products from "./components/pages/products/Products"
+const Dashboard = lazy(() => import("./components/pages/dashboard/Dashboard"))
+const Roles = lazy(() => import("./components/pages/roles/Roles"))
+const HomeLayout = lazy(() => import("./components/layout/HomeLayout"))
+const Permissions = lazy(() => import("./components/pages/permissions/Permissions"))
+const Profiles = lazy(() => import("./components/pages/profiles/Profiles"))
+const Settings = lazy(() => import("./components/pages/settings/Settings"))
+const BulkUpload = lazy(() => import("./components/pages/bulkupload/BulkUpload"))
+const Products = lazy(() => import("./components/pages/products/Products"))
 
 
 const App = () => {
@@ -49,8 +37,6 @@ const App = () => {
     <GoogleOAuthProvider clientId="247734515468-1caral148vs24evoub2at4hb7ncugtb4.apps.googleusercontent.com">
       <ThemeProvider theme={theme}>
         <div>
-
-
           <Suspense fallback={<Loader />}>
             <Routes>
               <Route
@@ -64,6 +50,7 @@ const App = () => {
                 <Route path="/settings" element={<PrivateRoute token={token} component={Settings} />} />
                 <Route path="/bulk-upload" element={<PrivateRoute token={token} component={BulkUpload} />} />
                 <Route path="/products" element={<PrivateRoute token={token} component={Products} />} />
+                <Route path='*' element={<Error />} />
               </Route>
               {/* <Route> */}
               {/* Public Routes */}
